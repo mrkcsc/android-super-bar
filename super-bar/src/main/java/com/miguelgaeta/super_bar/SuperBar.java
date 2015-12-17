d40e19153c5499ac81f1c585eeed84f3d39dee16
mrkcsc <miguel@miguelgaeta.com>
2015-12-17 15:24:00 -0800

    Move out drawing related functions into drawer class.

--- super-bar/src/main/java/com/miguelgaeta/super_bar/SuperBar.java
+++ super-bar/src/main/java/com/miguelgaeta/super_bar/SuperBar.java
@@ -3,7 +3,6 @@ package com.miguelgaeta.super_bar;
 import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
@@ -82,19 +81,19 @@ public class SuperBar extends View implements ValueAnimator.AnimatorUpdateListen
         void onSelectionMoved(float value, float maxValue, float minValue, SuperBar superBar);
     }
 
-    private final SuperBarAttributes attrs = new SuperBarAttributes(this);
+    final SuperBarAttributes attrs = new SuperBarAttributes(this);
 
-    private final SuperBarPainter paint = new SuperBarPainter(this);
+    final SuperBarPainter paint = new SuperBarPainter(this);
 
-    private final SuperBarConfig config = new SuperBarConfig(this);
+    final SuperBarDrawer drawer = new SuperBarDrawer(this);
+
+    final SuperBarConfig config = new SuperBarConfig(this);
 
     @SuppressWarnings("unused")
     public SuperBarConfig getConfig() {
 
         return config;
     }
-
-    private RectF mBar = new RectF();
 
     public SuperBar(Context context) {
         super(context);
@@ -117,9 +116,7 @@ public class SuperBar extends View implements ValueAnimator.AnimatorUpdateListen
 
         init();
     }
-    /**
-     * Do all preparations.
-     */
+
     private void init() {
 
         setLayerType(View.LAYER_TYPE_SOFTWARE, null);
@@ -149,58 +146,7 @@ public class SuperBar extends View implements ValueAnimator.AnimatorUpdateListen
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        final float shadowRadius = config.getControlShadowSize() / 2f;
-
-        final float halfMargin = config.getBarMargin() / 2f;
-
-        final float controlRadius = (getHeight() / 2f);
-
-        float barTop = shadowRadius + halfMargin;
-        float barBot = getHeight() - shadowRadius - halfMargin;
-
-        drawBackgroundBar(canvas, barTop, barBot, controlRadius);
-
-        drawBar(canvas, barTop, barBot, controlRadius, config.getBarValue());
-
-        final float controlX = mBar.right;
-
-        drawOverlayBar(canvas, barTop, barBot, controlRadius, config.getOverlayBarValue());
-
-        paint.setColor(config.getControlColor(), shadowRadius, config.getControlShadowColor());
-
-        // Dragging control.
-        canvas.drawCircle(controlX, (getHeight() / 2f), controlRadius - shadowRadius, paint);
-    }
-
-    private void drawBar(Canvas canvas, float barTop, float barBot, float controlRadius, float barValue) {
-
-        float length = ((getWidth() - (controlRadius * 2)) / (config.getMaxBarValue() - config.getMinBarValue())) * (barValue - config.getMinBarValue());
-
-        mBar.set(controlRadius, barTop, length + controlRadius, barBot);
-
-        paint.setColor(config.getColor().getColor(config.getBarValue(), config.getMaxBarValue(), config.getMinBarValue()));
-
-        canvas.drawRoundRect(mBar, mBar.height() / 2f, mBar.height() / 2f, paint);
-    }
-
-    private void drawBackgroundBar(Canvas canvas, float barTop, float barBot, float controlRadius) {
-
-        mBar.set(controlRadius, barTop, getWidth() - controlRadius, barBot);
-
-        paint.setColor(config.getBackgroundColor());
-
-        canvas.drawRoundRect(mBar, mBar.height() / 2f, mBar.height() / 2f, paint);
-    }
-
-    private void drawOverlayBar(Canvas canvas, float barTop, float barBot, float controlRadius, float barValue) {
-
-        float length = ((getWidth() - (controlRadius * 2)) / (config.getMaxBarValue() - config.getMinBarValue())) * (barValue - config.getMinBarValue());
-
-        mBar.set(length + controlRadius, barTop, getWidth() - controlRadius, barBot);
-
-        paint.setColor(config.getOverlayBarColor().getColor(config.getOverlayBarValue(), config.getMaxBarValue(), config.getMinBarValue()));
-
-        canvas.drawRoundRect(mBar, mBar.height() / 2f, mBar.height() / 2f, paint);
+        drawer.draw(canvas);
     }
 
     @Override
@@ -212,77 +158,6 @@ public class SuperBar extends View implements ValueAnimator.AnimatorUpdateListen
     @Override
     public boolean onTouchEvent(MotionEvent motionEvent) {
 
-        if (!config.isTouchEnabled()) {
-
-            return super.onTouchEvent(motionEvent);
-        }
-
-        if (config.getGestureDetector() != null &&
-            config.getGestureDetector().onTouchEvent(motionEvent)) {
-
-            return true;
-        }
-
-        float x = motionEvent.getX();
-
-        switch (motionEvent.getAction()) {
-
-            case MotionEvent.ACTION_DOWN:
-                updateValue(x);
-                invalidate();
-            case MotionEvent.ACTION_MOVE:
-                updateValue(x);
-                invalidate();
-
-                if (config.getOnSelectionMoved() != null) {
-                    config.getOnSelectionMoved().onSelectionMoved(config.getBarValue(), config.getMaxBarValue(), config.getMinBarValue(), this);
-                }
-
-                break;
-            case MotionEvent.ACTION_UP:
-                updateValue(x);
-                invalidate();
-
-                if (config.getOnSelectionChanged() != null) {
-                    config.getOnSelectionChanged().onSelectionChanged(config.getBarValue(), config.getMaxBarValue(), config.getMinBarValue(), this);
-                }
-
-                break;
-        }
-
-        return true;
-    }
-
-    /**
-     * Updates the value on the ValueBar depending on the touch position.
-     */
-    private void updateValue(float x) {
-
-        float newVal;
-
-        if (x <= 0)
-            newVal = config.getMinBarValue();
-        else if (x > getWidth())
-            newVal = config.getMaxBarValue();
-        else {
-            float factor = x / getWidth();
-
-            newVal = (config.getMaxBarValue() - config.getMinBarValue()) * factor + config.getMinBarValue();
-        }
-
-        if (config.getBarInterval() > 0f) {
-
-            float remainder = newVal % config.getBarInterval();
-
-            // check if the new value is closer to the next, or the previous
-            if (remainder <= config.getBarInterval() / 2f) {
-
-                newVal = newVal - remainder;
-            } else {
-                newVal = newVal - remainder + config.getBarInterval();
-            }
-        }
-
-        config.setBarValue(null, newVal);
+        return drawer.onTouchEvent(motionEvent);
     }
 }
---
+++ super-bar/src/main/java/com/miguelgaeta/super_bar/SuperBarDrawer.java
@@ -0,0 +1,168 @@
+package com.miguelgaeta.super_bar;
+
+import android.graphics.Canvas;
+import android.graphics.RectF;
+import android.view.MotionEvent;
+
+/**
+ * Handles drawing and update operations for the super bar.
+ *
+ * Created by Miguel Gaeta on 12/17/15.
+ */
+class SuperBarDrawer {
+
+    private final SuperBar sb;
+
+    SuperBarDrawer(SuperBar superBar) {
+
+        this.sb = superBar;
+    }
+
+    private RectF mBar = new RectF();
+
+    void draw(Canvas canvas) {
+
+        final float shadowRadius = sb.config.getControlShadowSize() / 2f;
+
+        final float halfMargin = sb.config.getBarMargin() / 2f;
+
+        final float controlRadius = (sb.getHeight() / 2f);
+
+        float barTop = shadowRadius + halfMargin;
+        float barBot = sb.getHeight() - shadowRadius - halfMargin;
+
+        drawBackgroundBar(canvas, barTop, barBot, controlRadius);
+
+        drawBar(canvas, barTop, barBot, controlRadius, sb.config.getBarValue());
+
+        final float controlX = mBar.right;
+
+        drawOverlayBar(canvas, barTop, barBot, controlRadius, sb.config.getOverlayBarValue());
+
+        sb.paint.setColor(sb.config.getControlColor(), shadowRadius, sb.config.getControlShadowColor());
+
+        // Dragging control.
+        canvas.drawCircle(controlX, (sb.getHeight() / 2f), controlRadius - shadowRadius, sb.paint);
+    }
+
+    private void drawBar(Canvas canvas, float barTop, float barBot, float controlRadius, float barValue) {
+
+        float length = ((sb.getWidth() - (controlRadius * 2)) / (sb.config.getMaxBarValue() - sb.config.getMinBarValue())) * (barValue - sb.config.getMinBarValue());
+
+        mBar.set(controlRadius, barTop, length + controlRadius, barBot);
+
+        sb.paint.setColor(sb.config.getColor().getColor(sb.config.getBarValue(), sb.config.getMaxBarValue(), sb.config.getMinBarValue()));
+
+        canvas.drawRoundRect(mBar, mBar.height() / 2f, mBar.height() / 2f, sb.paint);
+    }
+
+    private void drawBackgroundBar(Canvas canvas, float barTop, float barBot, float controlRadius) {
+
+        mBar.set(controlRadius, barTop, sb.getWidth() - controlRadius, barBot);
+
+        sb.paint.setColor(sb.config.getBackgroundColor());
+
+        canvas.drawRoundRect(mBar, mBar.height() / 2f, mBar.height() / 2f, sb.paint);
+    }
+
+    private void drawOverlayBar(Canvas canvas, float barTop, float barBot, float controlRadius, float barValue) {
+
+        float length = ((sb.getWidth() - (controlRadius * 2)) / (sb.config.getMaxBarValue() - sb.config.getMinBarValue())) * (barValue - sb.config.getMinBarValue());
+
+        mBar.set(length + controlRadius, barTop, sb.getWidth() - controlRadius, barBot);
+
+        sb.paint.setColor(sb.config.getOverlayBarColor().getColor(
+            sb.config.getOverlayBarValue(),
+            sb.config.getMaxBarValue(),
+            sb.config.getMinBarValue()));
+
+        canvas.drawRoundRect(mBar, mBar.height() / 2f, mBar.height() / 2f, sb.paint);
+    }
+
+    public boolean onTouchEvent(MotionEvent motionEvent) {
+
+        if (!sb.config.isTouchEnabled()) {
+
+            return sb.onTouchEvent(motionEvent);
+        }
+
+        if (sb.config.getGestureDetector() != null &&
+            sb.config.getGestureDetector().onTouchEvent(motionEvent)) {
+
+            return true;
+        }
+
+        float x = motionEvent.getX();
+
+        switch (motionEvent.getAction()) {
+
+            case MotionEvent.ACTION_DOWN:
+
+                updateValue(sb.config, x);
+
+            case MotionEvent.ACTION_MOVE:
+
+                updateValue(sb.config, x);
+
+                if (sb.config.getOnSelectionMoved() != null) {
+                    sb.config.getOnSelectionMoved().onSelectionMoved(
+                        sb.config.getBarValue(),
+                        sb.config.getMaxBarValue(),
+                        sb.config.getMinBarValue(), sb);
+                }
+
+                break;
+
+            case MotionEvent.ACTION_UP:
+
+                updateValue(sb.config, x);
+
+                if (sb.config.getOnSelectionChanged() != null) {
+                    sb.config.getOnSelectionChanged().onSelectionChanged(
+                        sb.config.getBarValue(),
+                        sb.config.getMaxBarValue(),
+                        sb.config.getMinBarValue(), sb);
+                }
+
+                break;
+        }
+
+        return true;
+    }
+
+    private void updateValue(SuperBarConfig config, float x) {
+
+        float newVal;
+
+        if (x <= 0) {
+            newVal = config.getMinBarValue();
+
+        } else if (x > sb.getWidth()) {
+
+            newVal = config.getMaxBarValue();
+
+        } else {
+
+            float factor = x / sb.getWidth();
+
+            newVal = (config.getMaxBarValue() - config.getMinBarValue()) * factor + config.getMinBarValue();
+        }
+
+        if (config.getBarInterval() > 0f) {
+
+            float remainder = newVal % config.getBarInterval();
+
+            // check if the new value is closer to the next, or the previous
+            if (remainder <= config.getBarInterval() / 2f) {
+
+                newVal = newVal - remainder;
+            } else {
+                newVal = newVal - remainder + config.getBarInterval();
+            }
+        }
+
+        config.setBarValue(null, newVal);
+
+        sb.invalidate();
+    }
+}
