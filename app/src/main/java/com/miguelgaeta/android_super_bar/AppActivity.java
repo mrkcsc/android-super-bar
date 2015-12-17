46ad7fa117da013cdf003b131d38895e7b8d9d97
mrkcsc <miguel@miguelgaeta.com>
2015-12-16 16:12:45 -0800

    Initial project commit.

---
+++ app/src/main/java/com/miguelgaeta/android_super_bar/AppActivity.java
@@ -0,0 +1,12 @@
+package com.miguelgaeta.android_super_bar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AppActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
